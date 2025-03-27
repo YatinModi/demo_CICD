9d968e8acda0f888566a69d8a7adf76374c54cc7
getyoteamsolutions <sales@getyoteam.com>
2025-03-27 11:38:09 +0530

    Check CICD with test

--- MommyMapUITests/MommyMapUITests.swift
+++ MommyMapUITests/MommyMapUITests.swift
@@ -13,6 +13,9 @@ final class MommyMapUITests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        let app = XCUIApplication()
+        app.launch()
+
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
